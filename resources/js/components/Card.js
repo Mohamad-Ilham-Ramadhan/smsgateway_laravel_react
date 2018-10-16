7918be6bb23ef5c497c95a1514b9e499646682c4
Mohamad Ilham Ramadhan <mohamadilhamramadhan@gmail.com>
2018-10-16 09:53:19 +0700

    create Card Component implement it in Dashboard page

--- resources/js/components/Card.js
+++ resources/js/components/Card.js
@@ -1,10 +1,21 @@
 import React from 'react';
 
-const Card = ({ className }) => {
+const Card = ({ className, title, extra, footer, children }) => {
     return (
-        <div className="card ">
-        	
-        </div>
+        <div className={"card " + className}>
+        	<div className="clearfix">
+        		<div className="card-title">{ title }</div>
+        		{extra}
+        	</div>
+			<div className="card-body">
+                {children}
+			</div>
+            { footer && 
+    			<div className="card-footer clearfix">
+                    {footer}
+    			</div>
+            }
+		</div>
     );
 };
 
--- resources/js/components/pages/Dashboard.js
+++ resources/js/components/pages/Dashboard.js
@@ -1,6 +1,7 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 import Chart from 'chart.js';
 
+import Card from '@/components/Card';
 
 class Dashboard extends Component {
 
@@ -9,6 +10,7 @@ class Dashboard extends Component {
 	}
 
 	componentDidMount() {
+		this.chart.responsive = true;
 		this.ctx = this.chart.getContext('2d')
 		this.smsChart = new Chart(this.ctx, {
 			type: 'horizontalBar',
@@ -18,10 +20,12 @@ class Dashboard extends Component {
 					backgroundColor: 'rgb(255, 99, 132)',
 					borderColor: 'rgb(255, 99, 132)',
 					data:[ 100, 200],
-					// data:[{{ total_inboxs_current_month }}, {{ total_sentitems_current_month }}],
+					// data:[{{ total
+					// _inboxs_current_month }}, {{ total_sentitems_current_month }}],
 				}]
 			},
 			options: {
+				maintainAspectRatio: false,
 		        legend: {
 		            display: false,
 		        },
@@ -56,26 +60,26 @@ class Dashboard extends Component {
 
 						<div className="row mt-5">
 							<div className="col">
-								<div className="card card-main">
-									<div className="card-title">
-										Trafik SMS Bulan Ini
+
+								<Card 
+									className="card-main"
+									title="Trafik SMS Bulan Ini"
+								>
+									<div className="position-relative" style={ {height: '200px'} }>
+										<canvas ref={this.setChart}></canvas>		
 									</div>
-									<canvas ref={this.setChart} height="200px"></canvas>		
-								</div>
+								</Card>
 							</div>
 						</div>
 
 						<div className="row">
 							<div className="col-lg-7">
-										
-								<div className="card card-main mt-5">
-									<div className="card-title">
-										<span className="far fa-comments mr-2"></span>
-										Inbox
-									</div>
-									<div id="newInboxs" className="card-body">
-									{/* loop */}
-										<div className="message">
+
+								<Card 
+									className="card-main mt-5"
+									title={<Fragment><span className="far fa-comments mr-2"></span>Inbox</Fragment>}
+								>
+									<div className="message">
 											<div className="message-icon d-inline-block">
 												<span className="far fa-user"></span>
 											</div>
@@ -85,39 +89,40 @@ class Dashboard extends Component {
 											</span>
 											<p className="message-text ml-5">{/*inbox.TextDecoded*/}</p>
 										</div>
-									</div>
-								</div>
+								</Card>	
 							</div>
 
 							<div className="col-lg-5">
-								<div className="card card-main mt-5">
-									<div className="card-title">
-										<span className="fas fa-envelope mr-2"></span>
-										Quick SMS
-									</div>
+								<Card
+									className="card-main mt-5"
+									title={<Fragment><span className="fas fa-envelope mr-2"></span>Quick SMS</Fragment>}
+									footer={
+										<button type="submit" className="btn float-right"><small>Send <span className="far fa-paper-plane ml-2"></span></small></button>
+									}
+								>
 									<form action="/dashboard/store" method="POST" className="form-quick-sms">
+
 										<input type="hidden" name="{{ csrf_token_name }}" value="{{ csrf_hash }}" />
-										<div className="card-body">
+
 											<div className="form-group">
 												<input name="DestinationNumber" type="number" className="form-control" placeholder="Phone number..." />
 											</div>
+											
 											<div className="sms-input-module">
 												
 												<div className="form-group">
 													<textarea name="TextDecoded" type="text" maxLength="160" className="sms-input form-control" placeholder="Write a message..." style={ {height: '150px'} }></textarea>
 												</div>
+
 												<div className="form-group">
 													<input data-char-left='1' type="number" className="sms-char-left form-control is-small d-inline-block" value="160" readOnly />
 													<small className="text-right">Sisa Karakter</small>
 												</div>
 												
 											</div>
-										</div>
-										<div className="card-footer clearfix">
-											<button type="submit" className="btn float-right"><small>Send <span className="far fa-paper-plane ml-2"></span></small></button>
-										</div>
+
 									</form>
-								</div>
+								</Card>
 							</div>
 						</div>
 
