80babedbbd3bbb49cc4dad6792d68137b2dcd5c7
Mohamad Ilham Ramadhan <mohamadilhamramadhan@gmail.com>
2018-10-16 10:33:41 +0700

    complete SendSMSSingle page (front-end | presentational)

--- resources/js/components/Card.js
+++ resources/js/components/Card.js
@@ -1,13 +1,13 @@
 import React from 'react';
 
-const Card = ({ className, title, extra, footer, children }) => {
+const Card = ({ className, title, extra, footer, cardBodyClassName, children }) => {
     return (
         <div className={"card " + className}>
         	<div className="clearfix">
         		<div className="card-title">{ title }</div>
         		{extra}
         	</div>
-			<div className="card-body">
+			<div className={"card-body " + cardBodyClassName}>
                 {children}
 			</div>
             { footer && 
