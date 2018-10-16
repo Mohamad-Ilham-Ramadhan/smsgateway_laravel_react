3db2b7b04b654926825dc30086e80cf80192b794
Mohamad Ilham Ramadhan <mohamadilhamramadhan@gmail.com>
2018-10-16 13:54:46 +0700

    fix extra props in <Card /> component

--- resources/js/components/Card.js
+++ resources/js/components/Card.js
@@ -1,11 +1,12 @@
-import React from 'react';
+import React, { cloneElement } from 'react';
 
 const Card = ({ className, title, extra, footer, bodyClassName, children }) => {
+    console.log( extra.props.className );
     return (
         <div className={"card " + className}>
-        	<div className="clearfix">
+        	<div className="clearfix position-relative">
         		<div className="card-title">{ title }</div>
-        		{extra}
+        		{cloneElement(extra, { style: {position: 'absolute', top: '0',  right: '0'} }) }
         	</div>
 			<div className={"card-body " + bodyClassName}>
                 {children}
